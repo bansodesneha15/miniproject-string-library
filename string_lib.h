233de6383daca8d07020e08df17db0c7bb94d1a4
bansodesneha15 <bansodess15.it@coep.ac.in>
2016-10-18 23:40:06 +0530

    Update string_lib.h

--- string_lib.h
+++ string_lib.h
@@ -1,5 +1,7 @@
 
 
+char *stpcpy1(char * , const char *);
+
 void string_catenate(char *, char *);
 
 void string_copy(char *, char *);
@@ -8,4 +10,20 @@ char* string_dup(char *);
 
 int string_length(char *);
 
+char *strchr1(const char *, int );
 
+char *strncpy1(char *, const char *, size_t);
+
+size_t strcspn1(const char *, register const char *);
+
+int strcmp1(const char *, const char *);
+
+char *stpncpy1(char *, const char *, size_t);
+
+char *strncat1(char *, const char *, register size_t );
+
+char *strstr1(const char *, const char *);
+
+int strcasecmp1 (const char *, const char *);
+
+int strncasecmp1(const char *, const char *, size_t);
